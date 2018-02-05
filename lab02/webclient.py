db7a856ff6de2667fa4b505a588c486d72e36263
Ying-Chun Lin <lin915@purdue.edu>
2018-02-05 11:00:47 -0500

    http server and client version 1.0

--- lab02/webclient.py
+++ lab02/webclient.py
@@ -1,23 +1,50 @@
-import http.client
+import socket
 import sys
 
-def main(server_ip, server_port, file_path):
 
-    connect_server = http.client.HTTPConnection(server_ip, int(server_port))
-    if file_path == '/':
-        file_path = '/index.html'
+def main(server_ip, port, file_name):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    connect_server.request('GET', file_path)
+    print('Connecting to {}:{}'.format(server_ip, port))
+    sock.connect((server_ip, port))
 
-    response = connect_server.getresponse()
+    if file_name == '/':
+        file_name = '/index.html'
 
-    print(response.status, response.reason)
-    rcv_data = response.read()
-    with open('./Download/' + file_path, 'wb') as fw:
-        fw.write(rcv_data)
+    try:
+        request = 'GET {}'.format(file_name)
 
-    print(rcv_data)
+        sock.sendall(bytes(request, encoding='utf-8'))
+        print('Requesting "{}"'.format(file_name))
+
+        resp_data = b''
+        while True:
+            data = sock.recv(1024)
+            if not data:
+                break
+            resp_data += data
+        output_data = b''
+        is_content = False
+        for line in resp_data.split(b'\n'):
+            if is_content:
+                output_data += line
+            if line == '':
+                is_content = True
+        print(resp_data)
+        with open('./Download' + file_name, 'wb') as fw:
+            fw.write(resp_data)
+
+    finally:
+        print('Close socket')
+        sock.close()
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+
+
+
+
+
+
+
--- lab02/webserver.py
+++ lab02/webserver.py
@@ -1,47 +1,96 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
 import sys
-#import thread
+import threading
+import time
 
 
-class SimpleServer(BaseHTTPRequestHandler):
+class SimpleServer():
 
-    def _set_headers(self, file):
-        self.send_response(200)
-        if file.split('.')[-1] == 'jpeg':
-            self.send_header('Content-type','image/jpeg')
-        elif file.split('.')[-1] == 'gif':
-            self.send_header('Content-type', 'image/gif')
-        else:
-            self.send_header('Content-type', 'text/html')
-        self.end_headers()
+    def __init__(self, server_ip, server_port):
+        self.root_path = './Upload'
+        self.ip = server_ip
+        self.port = server_port
 
-    def do_GET(self):
+    def active_server(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
         try:
-            self.root_path = './Upload'
+            self.sock.bind((self.ip, self.port))
+            print('Server Active...')
+        except Exception:
+            print('ERROR: Failed to bind sockets for {}:{}'.format(self.ip, self.port))
+            self.shutdown()
+            sys.exit(1)
 
-            if self.path == '/':
-                self.path = '/index.html'
+        self._start_listen()
 
-            fr = open(self.root_path + self.path, 'rb')
-            self._set_headers(self.path)
+    def _start_listen(self):
+        self.sock.listen(5)
+        threads = []
+        while True:
+            connection, client_addr = self.sock.accept()
+            t = threading.Thread(target=self._new_client_handler, args=(connection, client_addr))
+            threads.append(t)
+            t.start()
 
-            self.wfile.write(bytes(fr.read()))
+    def _new_client_handler(self, client_sock, client_addr):
+        print(sys.stderr, 'New client {}:{}'.format(client_addr[0], client_addr[1]))
+
+        data = client_sock.recv(1024)
+        request = bytes.decode(data)
+        request_method = request.split(' ')[0]
+        print("Method: ", request_method)
+        print("Request body: ", request)
+
+        if request_method == 'GET':
+            request_file = request.split(' ')[1]
+            self._do_get(request_file, client_sock)
+
+        client_sock.close()
+
+    def shutdown(self):
+        self.sock.socket.shutdown(socket.SHUT_RDWR)
+        print('Shutting down the server...')
+
+    def _set_headers(self, code, file):
+        server_header = ''
+        if code == 200:
+            server_header = 'HTTP/1.1 200 OK\n'
+        elif code == 404:
+            server_header = 'HTTP/1.1 404 Not Found\n'
+
+        current_time = time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime())
+        server_header += 'Date: {}\n'.format(current_time)
+        server_header += 'Server: Simple-Server\n'
+        server_header += 'Connection: close\n\n'
+
+        if (file.split('.')[-1] == 'jpeg' or file.split('.')[-1] == 'gif') and code == 200:
+            server_header = ''
+        return bytes(server_header, encoding='utf-8')
+
+    def _do_get(self, path, conn):
+        try:
+            if path == '/':
+                path = '/index.html'
+
+            fr = open(self.root_path + path, 'rb')
+            response_content = fr.read()
             fr.close()
-
-            return
+            server_resp = self._set_headers(200, path)
+            server_resp += response_content
         except IOError:
-            self.send_error(404, 'File not found')
+            server_resp = self._set_headers(404, path)
+
+        conn.send(server_resp)
 
 
-
-
-def run(server_class=HTTPServer, handler_class=SimpleServer, port = 1119):
-    server_addr = ('127.0.0.1', port)
-    httpd = server_class(server_addr, handler_class)
-    print('Server is running...')
-    httpd.serve_forever()
+def main(server_ip, server_port):
+    s = SimpleServer(server_ip, server_port)
+    s.active_server()
 
 
 if __name__ == '__main__':
-    run(port=int(sys.argv[1]))
+    main('127.0.0.1', int(sys.argv[1]))
+
 
